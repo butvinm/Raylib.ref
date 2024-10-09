9ce15ac16273515c4623d778233b5a6b511ed538
Mihail Butvin <butvin.mihail@yandex.ru>
2024-10-09 10:08:25 +0300

    Use not a shity compiler

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ rlmake \
     --dont-keep-rasl \
     --tmp-dir build \
     -o build/Main \
-    -c "g++ -I./raylib/include -L./raylib/lib -Wl,-rpath,./raylib/lib -l raylib -lGL -lm -lpthread -ldl -lrt -lX11 -o" \
+    -c "clang++ -I./raylib/include -L./raylib/lib -Wl,-rpath,./raylib/lib -l raylib -lGL -lm -lpthread -ldl -lrt -lX11 -o" \
     Main.ref
